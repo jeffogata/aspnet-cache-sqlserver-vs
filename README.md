91787461918e3dd60cf05abf22faab1d6f284029
Jeff Ogata <jeffogata@outlook.com>
2016-04-16 19:37:11 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# aspnet-cache-sqlserver-vs
+
+Caching with ASP.NET Core, accompanying the [ASP.NET Core Caching](https://jeffogata.com/asp-net-core-caching/) blog post.
+
+Tested with the following:
+* ASP.NET 5 RC1 Update1
+* Visual Studio 2015, Update 1
+* Windows 10
+* SQL Server 2014
---
+++ src/AspNetCacheSqlServer/Startup.cs
@@ -0,0 +1,111 @@
+﻿namespace AspNetCacheSqlServer
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.AspNet.Builder;
+    using Microsoft.AspNet.Http;
+    using Microsoft.Extensions.Caching.Distributed;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSqlServerCache(opt =>
+            {
+                opt.ConnectionString = @"server=DESKTOP-QEH823S\SQL2014;database=Test;trusted_connection=true";
+                opt.SchemaName = "dbo";
+                opt.TableName = "Cache";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IDistributedCache cache)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("ASP.NET Core:  SQL Server Cache");
+                await next();
+            });
+
+            var cacheOptions = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddYears(1)
+            };
+
+            app.Map(new PathString("/set"), branch =>
+            {
+                branch.Run(async context =>
+                {
+                    foreach (var value in context.Request.Query)
+                    {
+                        await cache.SetAsync(value.Key, Encoding.UTF8.GetBytes(value.Value.First()), cacheOptions);
+                    }
+
+                    await context.Response.WriteAsync("<br>values set");
+                });
+            });
+
+            app.Map(new PathString("/get"), branch =>
+            {
+                branch.Run(async context =>
+                {
+                    var keys = context.Request.Query["key"];
+
+                    foreach (var key in keys)
+                    {
+                        var value = await cache.GetAsync(key);
+                        var valueString = value == null ? "(not found)" : Encoding.UTF8.GetString(value);
+                        await context.Response.WriteAsync($"<br>{key} :: {valueString}");
+                    }
+                });
+            });
+
+            app.Map(new PathString("/del"), branch =>
+            {
+                branch.Run(async context =>
+                {
+                    var keys = context.Request.Query["key"];
+
+                    foreach (var key in keys)
+                    {
+                        await cache.RemoveAsync(key);
+                    }
+
+                    await context.Response.WriteAsync("<br>values removed");
+                });
+            });
+
+            app.Map(new PathString("/perf"), branch =>
+            {
+                branch.Run(async context =>
+                {
+                    var key = Guid.NewGuid().ToString();
+                    var value = Guid.NewGuid().ToByteArray();
+
+                    await cache.SetAsync(key, value, cacheOptions);
+
+                    Guid cacheValue;
+
+                    var reps = 10000;
+
+                    var sw = Stopwatch.StartNew();
+
+                    for (var i = 0; i < reps; i++)
+                    {
+                        cacheValue = new Guid(await cache.GetAsync(key));
+                    }
+
+                    sw.Stop();
+
+                    await
+                        context.Response.WriteAsync(
+                            $"<br>average time to retrieve value: {sw.ElapsedTicks/(decimal) reps} ticks");
+                });
+            });
+        }
+    }
+}
